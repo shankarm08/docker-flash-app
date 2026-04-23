c90814ceb8d2ed54d9efc0204867a48f77e06a36
shankar <shankarm2021@gmail.com>
2026-04-23 23:34:03 +0530

    update app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Docker Flask App!"
+    return "🚀 Hello from Docker Flask App! v2"
 
 @app.route("/health")
 def health():
