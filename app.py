b2ee4f036f73ed684b49d7037ba5cdd66cf55e66
shankar <shankarm2021@gmail.com>
2026-04-25 17:04:21 +0530

    trigger testing updated eleven

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Docker Flask App! v10"
+    return "🚀 Hello from Docker Flask App! v12"
 
 @app.route("/health")
 def health():
