30408b68e77ae80c0e882461753b763446d9e7e0
shankar <shankarm2021@gmail.com>
2026-04-25 11:37:38 +0530

    this is my thrid commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Docker Flask App! v2"
+    return "🚀 Hello from Docker Flask App! v3"
 
 @app.route("/health")
 def health():
